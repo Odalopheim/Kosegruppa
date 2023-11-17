5efb4a7bf349162e82bae3add3d4df1bc19aecf5
olivgyv <olivercg@uia.no>
2023-11-17 15:48:58 +0100

    Create oliver.js

---
+++ js/oliver.js
@@ -0,0 +1,8 @@
+circle(100,"solid", "crimson", 100, 100)
+circle(80, "solid", "blue", 100, 100)
+rectangle(112, 112, 45, 44, "solid", "firebrick" )
+rectangle(80,100,50,60,"solid","black")
+rectangle(80,60,70,60,"solid","white")
+circle(30,"solid","black", 100,100)
+circle(10,"solid","gray", 100,100)
+
