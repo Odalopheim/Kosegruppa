9112a0c2bd10e237ed0a82de019b0ade930d76d8
vebjornkjus <vebjornfk@uia.no>
2023-11-21 15:44:03 +0100

    Create vebjorn.js

---
+++ js/vebjorn.js
@@ -0,0 +1,36 @@
+
+
+
+// Canvas
+drawRectangle(200, 200, 0, 100, "solid", "green");
+drawRectangle(185, 185, 7.5, 107.5, "solid", "darkblue");
+drawRectangle(170, 170, 15, 115, "solid", "purple");
+drawRectangle(155, 155, 22.5, 122.5, "solid", "orange");
+drawRectangle(140, 140, 30, 130, "solid", "crimson");
+drawRectangle(125, 125, 37.5, 137.5, "solid", "hotpink");
+drawRectangle(110, 110, 45, 145, "solid", "yellow");
+drawRectangle(95, 95, 52.5, 152.5, "solid", "indigo");
+drawRectangle(80, 80, 60, 160, "solid", "red");
+drawRectangle(65, 65, 67.5, 167.5, "solid", "dodgerblue");
+drawRectangle(50, 50, 75, 175, "solid", "orchid");
+drawRectangle(35, 35, 82.5, 182.5, "solid", "aqua");
+
+
+// SVG
+rectangleSVG(0, 0, 200, 200, "fill:rgb(0, 128, 0)", "green");
+rectangleSVG(7.5, 7.5, 185, 185, "fill:rgb(0, 0, 139)", "darkblue");
+rectangleSVG(15, 15, 170, 170, "fill:rgb(128, 0, 128)", "purple");
+rectangleSVG(22.5, 22.5, 155, 155, "fill:rgb(255, 165, 0)", "orange");
+rectangleSVG(30, 30, 140, 140, "fill:rgb(220, 20, 60)", "crimson");
+rectangleSVG(37.5, 37.5, 125, 125, "fill:rgb(255, 105, 180)", "hotpink");
+rectangleSVG(45, 45, 110, 110, "fill:rgb(255, 255, 0)", "yellow");
+rectangleSVG(52.5, 52.5, 95, 95, "fill:rgb(75, 0, 130)", "indigo");
+rectangleSVG(60, 60, 80, 80, "fill:rgb(255, 0, 0)", "red");
+rectangleSVG(67.5, 67.5, 65, 65, "fill:rgb(30, 144, 255)", "dodgerblue");
+rectangleSVG(75, 75, 50, 50, "fill:rgb(218, 112, 214)", "orchid");
+rectangleSVG(82.5, 82.5, 35, 35, "fill:rgb(0, 255, 255)", "aqua");
+
+
+
+
+
