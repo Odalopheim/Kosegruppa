4f82649c63cdd005028774efd785bc2eaa161f65
olivgyv <olivercg@uia.no>
2023-11-21 15:43:47 +0100

    Update oliver.js

--- js/oliver.js
+++ js/oliver.js
@@ -1,3 +1,4 @@
+//Canvas
 circle(100,"solid", "crimson", 100, 100)
 circle(80, "solid", "blue", 100, 100)
 drawRectangle(112, 112, 45, 44, "solid", "firebrick" )
@@ -5,7 +6,7 @@ drawRectangle(80,100,50,60,"solid","black")
 drawRectangle(80,60,70,60,"solid","white")
 circle(30,"solid","black", 100,100)
 circle(10,"solid","gray", 100,100)
-
+//SVG
 circleSVG(200,100,100,"fill:rgb(220,20,60)");
 circleSVG(200,100,80,"fill:rgb(0,0,255)");
 rectangleSVG(145,44,112,112,"fill:rgb(178,34,34)");
