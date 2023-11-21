7fbf963724f027d77de2939386c208991b8cb824
Kajahebnes <144679830+Kajahebnes@users.noreply.github.com>
2023-11-21 16:57:09 +0100

    Create Kaja.js

---
+++ js/Kaja.js
@@ -0,0 +1,23 @@
+
+// Canvas
+drawRectangle(160, 220, 50, 50, "solid", "orange" )
+drawRectangle(67, 130, 140, 135, "solid", "aqua" )
+drawRectangle(67, 131, 139, 50, "solid", "purple" )
+drawRectangle(67, 67, 50, 50, "solid", "yellow" )
+drawRectangle(67, 67, 50, 140, "solid", "pink" )
+drawRectangle(60, 127, 143, 50, "solid", "blue" )
+drawRectangle(70, 125, 145, 140, "solid", "green" )
+drawRectangle(67, 60, 50, 145, "solid", "coral" )
+drawRectangle(60, 60, 50, 50, "solid", "dark pink" )
+
+// SVG
+rectangleSVG(50, 50, 220, 160, "fill:rgb(255, 165, 0)");
+rectangleSVG(140, 135, 130, 67, "fill:rgb(0, 255, 255)");
+rectangleSVG(139, 50, 131, 67, "fill:rgb(128, 0, 128)");
+rectangleSVG(50, 50, 67, 67, "fill:rgb(255, 255, 0)");
+rectangleSVG(50, 140, 67, 67, "fill:rgb(255, 192, 203)");
+rectangleSVG(143, 50, 127, 60, "fill:rgb(0, 0, 255)");
+rectangleSVG(145, 140, 125, 70, "fill:rgb(0, 255, 0)");
+rectangleSVG(50, 145, 60, 67, "fill:rgb(255, 99, 71)");
+rectangleSVG(50, 50, 60, 60, "fill:rgb(255, 105, 180)");
+
