173f59dbb5dc9b61ec146c0a32d509f82307a27d
olivgyv <olivercg@uia.no>
2023-11-22 18:42:30 +0100

    Update figures.js

--- js/figures.js
+++ js/figures.js
@@ -1,5 +1,6 @@
 
-
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 // funksjon for rektangel
 function drawRectangle(height, width, x, y, mode, color) {
   ctx.fillStyle = color;
