f95b2f0824c994b0822e24cc19634973c21598d6
vebjornkjus <vebjornfk@uia.no>
2023-11-22 13:42:08 +0100

    Create mikke.js

---
+++ js/mikke.js
@@ -0,0 +1,14 @@
+
+// Canvas
+drawRectangle(103, 153, 47.5, 35, "solid", "black")
+drawRectangle(100, 150, 49, 36.5, "solid", "red")
+circle(51.5, "solid", "black", 125, 86.5)
+circle(50, "solid", "white", 125, 86.5)
+circle(25, "solid", "red", 125, 86.5)
+
+// SVG
+rectangleSVG(47.5, 35, 153, 103, "fill:rgb(0 ,0 ,0)");
+rectangleSVG(49, 36.5, 150, 100, "fill:rgb(255 ,0 ,0)");
+circleSVG(125, 86.5, 51.5, "fill:rgb(0,0,0)");
+circleSVG(125, 86.5, 50,"fill:rgb(255,255,255)");
+circleSVG(125, 86.5, 25,"fill:rgb(255,0,0)")
