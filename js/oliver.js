f8db75262548f8b980d05371c7c8ba867000143a
olivgyv <olivercg@uia.no>
2023-11-22 14:12:37 +0100

    Update oliver.js

--- js/oliver.js
+++ js/oliver.js
@@ -7,10 +7,10 @@ drawRectangle(80,60,70,60,"solid","white")
 circle(30,"solid","black", 100,100)
 circle(10,"solid","gray", 100,100)
 //SVG
-circleSVG(200,100,100,"fill:rgb(220,20,60)");
-circleSVG(200,100,80,"fill:rgb(0,0,255)");
-rectangleSVG(145,44,112,112,"fill:rgb(178,34,34)");
-rectangleSVG(150,60,100,80,"fill:rgb(0,0,0)");
-rectangleSVG(170,60,60,80,"fill:rgb(255,255,255)");
-circleSVG(200,100,30,"fill:rgb(0,0,0)");
-circleSVG(200,100,10,"fill:rgb(128,128,128)");
+circleSVG(100,100,100,"fill:rgb(220,20,60)");
+circleSVG(100,100,80,"fill:rgb(0,0,255)");
+rectangleSVG(45,44,112,112,"fill:rgb(178,34,34)");
+rectangleSVG(50,60,100,80,"fill:rgb(0,0,0)");
+rectangleSVG(70,60,60,80,"fill:rgb(255,255,255)");
+circleSVG(100,100,30,"fill:rgb(0,0,0)");
+circleSVG(100,100,10,"fill:rgb(128,128,128)");
