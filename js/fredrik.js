0d14dc858e8bd37ed6a934d37a848d9624530c89
FredrikHusebruh <144796146+FredrikHusebruh@users.noreply.github.com>
2023-11-22 17:17:56 +0100

    Update fredrik.js

--- js/fredrik.js
+++ js/fredrik.js
@@ -1,13 +1,12 @@
 //Canvas
-// Bakrunn 
+// Bakrunn
+scale(1/2, 1/2)
 drawRectangle(400, 400, 1, 1, "Solid", "white")
 
 //cylinder skygge
 scale(1, 1/2)
 
 rot(13)
-//drawRectangle(70, 40, 170, 135,  "solid", "grey")
-//ellipse("grey", 0, 190, 136, 20, 6, 0, Math.PI * 2)
 drawRectangle(140, 40, 225, 300,  "solid", "grey")
 ellipse("grey", 0, 245, 300, 20, 12, 0, Math.PI * 2)
 rot(-13)
@@ -19,16 +18,18 @@ drawRectangle(140, 40, 120, 100,  "solid", "rgb(0, 0, 210)")
 ellipse("rgb(0, 0, 153)", 0, 140, 100, 20, 12, 0, Math.PI * 2)
 ellipse("rgb(0, 0, 210)", 0, 140, 240, 20, 12, 0, Math.PI * 2)
 drawRectangle(120, 5, 130, 120, "solid", "rgba(255, 255, 255, 0.2)" )
+scale(2, 2)
 
-//triangle
-triangles(300, 267, 300, 227, 300, 317, 80, "rgb(224, 0, 0)", "v")
-triangles(300, 180, 300, 140, 300, 220, 80, "rgb(153, 0, 0)", "v")
-triangles(299, 224, 299, 184, 299, 264, 80, "rgb(224, 0, 0)", "h")
+//svg
+//bakgrunn
+rectangleSVG("1", "1", "200", "200","fill:rgb(255, 255, 255)");
 
+//Skygge
+rectangleSVG(64, 90, 20, 40, "fill:grey", "solid", "13", "", "");
+createEllipse(78, 91, 10, 3, "fill: grey; stroke: black; stroke-width: 0;", 13);
 
-triangles(304, 309, 340, 160, 304, 170, 80, "grey", "v", "j")
-
-
-
-
-
+//Cylinder
+rectangleSVG("60", "50", "20", "79","fill:rgb(0, 0, 210)");
+rectangleSVG("65", "60", "3", "60","fill:rgba(255, 255, 255, 0.2)");
+createEllipse(70, 50, 10, 6, "fill:rgb(0, 0, 153);");
+createEllipse(70, 130, 10, 6, "fill:rgb(0, 0, 210);");
